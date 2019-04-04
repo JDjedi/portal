0afbec861b9ebae2c7b76b707058bb0647b9236a
JDjedi <jcdiaz1201@gmail.com>
2019-04-04 12:41:26 -0500

    node.http.connect play

--- server/main.js
+++ server/main.js
@@ -1,11 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+
+
 import '../imports/api/users';
 import '../imports/api/links';
-import SimpleSchema from 'simpl-schema'; 
+import '../imports/startup/simple-schema-config.js';
 
 
 Meteor.startup(() => {  // code to run on server at startup
-
+	// WebApp.connectHandlers.use((req, res, next) => {
+	// 	console.log('My custom middleware!');
+	// 	console.log(req.url, req.method, req.headers, req.query);
+	// 	next(); // allows the server to process the request and complete the process
+	// })
 });
 
-
+// *** pattern ***
+// req comes in
+// run our middleware one at a time
+// send them that page
